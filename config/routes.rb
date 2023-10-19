d72d93a305b53dacc51a9e099b138d487dfc1ae6
Dylan Timmons <dylanstimmons@icloud.com>
2023-10-18 18:59:08 -0600

    add routes for find

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        resources :find, only: [:index], controller: "search"
+      end
+      namespace :items do
+        resources :find_all, only: [:index], controller: "search"
+      end
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: "merchants/items"
       end
