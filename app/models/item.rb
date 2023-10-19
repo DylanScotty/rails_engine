34fa4be6e1f356b9f5dc5f11a615db6909587c0d
Dylan Timmons <dylanstimmons@icloud.com>
2023-10-18 18:58:20 -0600

    adds search method

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
     belongs_to :merchant
 
+    def self.find_all_items_by_name(params)
+        where("name ILIKE ?", "%#{params}%")
+    end
+
+    def self.find_all_items_by_price(params)
+        where("unit_price >= #{params}")
+    end
+
 end
