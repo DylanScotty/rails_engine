2af42574f602715d938300dbd52703acf687d504
Dylan Timmons <dylanstimmons@icloud.com>
2023-10-18 18:59:37 -0600

    adds test for merchant methods

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '::find_merchant_by_name' do
+    it 'returns the merchant based off the search result' do
+      @merchant_1 = Merchant.create!(name: "Billy")
+      @merchant_2 = Merchant.create!(name: "Jilly")
+      @merchant_3 = Merchant.create!(name: "Tilly")
+
+
+      expect(Merchant.find_merchant_by_name("Bil")).to eq(@merchant_1)
+    end
+  end
 end
